c8cc23aac537ef0897882a538041d5c93de6cd79
Bombsztyl <kacpergumieniuk@gmail.com>
2020-10-21 18:23:43 +0200

    games number

--- lichess/script.js
+++ lichess/script.js
@@ -1,8 +1,19 @@
 // https://lichess.org/api
 
-const url = "https://lichess.org/api/user/kamuimaru";
-let data = fetch(url).then(function(response) {
-    return response.json();
-})
-console.log(data);
+//const url = "https://lichess.org/api/user/kamuimaru";
+//let data = fetch(url).then(function(response) {
+//    return response.json();
+//})
+//console.log(data);
 //how to get it to work
+
+const urlBomb = "https://lichess.org/api/user/bombeczka"
+
+async function numberOfGames(){
+    const result = await fetch(urlBomb);
+    const bData = await result.json();
+    let rapidGames = bData.perfs.rapid.games
+    document.getElementById("number").innerHTML = rapidGames;
+}
+
+numberOfGames();
